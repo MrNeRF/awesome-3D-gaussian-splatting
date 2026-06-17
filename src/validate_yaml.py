516437d1a4525bc969729e05f881dd106b55774d
FeiFan Luo <144598410+chenjiadie@users.noreply.github.com>
2026-06-17 15:19:12 +0800

    添加我们组的工作

--- src/validate_yaml.py
+++ src/validate_yaml.py
@@ -32,7 +32,7 @@ allowed_tags = [
     "Medicine", "Meshing", "Misc", "Monocular", "Perspective-correct", "Object Detection", 
     "Optimization", "Physics", "Point Cloud", "Poses", "Project", "Ray Tracing", 
     "Rendering", "Relight", "Review", "Robotics", "Segmentation", "SLAM", "Sparse", "Super Resolution",
-    "Stereo", "Style Transfer", "Texturing", "Transformer", "Uncertainty", "Video", 
+    "Stereo", "Style Transfer", "Texturing", "Transformer", "Uncertainty", "Video", "Wireless",
     "Virtual Reality", "World Generation"
 ]
 
